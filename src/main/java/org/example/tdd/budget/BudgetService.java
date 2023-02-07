a7cdc6bc973aacbba52727ce08f0a7a45f2c57d3
Ryan Lee <ryan.lee@hy-tech.com.tw>
2023-02-07 22:48:52 +0800

    before refactor

--- src/main/java/org/example/tdd/budget/BudgetService.java
+++ src/main/java/org/example/tdd/budget/BudgetService.java
@@ -1,10 +1,54 @@
 package org.example.tdd.budget;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
+import org.example.tdd.budget.model.Budget;
+import org.example.tdd.budget.repo.IBudgetRepo;
 
 public class BudgetService {
 
-  int queryBudget(LocalDate startLocalDate, LocalDate endLocalDate) {
-    return 0;
+  private IBudgetRepo budgetRepo;
+
+  public BudgetService(IBudgetRepo budgetRepo) {
+    this.budgetRepo = budgetRepo;
+  }
+
+  public int queryBudget(LocalDate startLocalDate, LocalDate endLocalDate) {
+    if (startLocalDate.isAfter(endLocalDate)) {
+      throw new IllegalArgumentException("時間格式錯誤");
+    }
+    List<Budget> budgets = budgetRepo.findAll();
+
+    int total = 0;
+    if (CollectionUtils.isNotEmpty(budgets)) {
+      for (Budget budget : budgets) {
+        YearMonth yearMonth = YearMonth.parse(budget.getYearMonth(),
+            DateTimeFormatter.ofPattern("yyyyMM"));
+        LocalDate firstDate = null;
+        LocalDate endDate = null;
+        if (yearMonth.atDay(1).isBefore(startLocalDate)) {
+          firstDate = startLocalDate;
+        }else{
+          firstDate = yearMonth.atDay(1);
+        }
+
+        if (yearMonth.atEndOfMonth().isBefore(endLocalDate)) {
+          endDate = yearMonth.atEndOfMonth();
+        }else{
+          endDate = endLocalDate;
+        }
+        if(firstDate.isAfter(endDate)){
+          continue;
+        }
+        int between = (int) ChronoUnit.DAYS.between(firstDate, endDate) + 1;
+        int perDay = budget.getAmount() / yearMonth.lengthOfMonth();
+        total += between * perDay;
+      }
+    }
+    return total;
   }
 }
--- src/test/java/test/tdd/budget/BudgetTest.java
+++ src/test/java/test/tdd/budget/BudgetTest.java
@@ -1,5 +1,153 @@
 package test.tdd.budget;
 
-public class BudgetTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import org.example.tdd.budget.BudgetService;
+import org.example.tdd.budget.model.Budget;
+import org.example.tdd.budget.repo.IBudgetRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class BudgetTest {
+
+  private BudgetService budgetService;
+  private IBudgetRepo budgetRepo;
+
+  @BeforeEach
+  void setUp() {
+    budgetRepo = Mockito.mock(IBudgetRepo.class);
+    budgetService = new BudgetService(budgetRepo);
+  }
+
+  @Test
+  void test_wrong_time() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 7, 25);
+    LocalDate endDate = LocalDate.of(2022, 7, 5);
+
+    // Act
+    IllegalArgumentException exception = Assertions.assertThrows(
+        IllegalArgumentException.class,
+        () -> budgetService.queryBudget(startDate, endDate)
+    );
+
+    // Assert
+    assertEquals("時間格式錯誤", exception.getMessage());
+  }
+
+  @Test
+  void test_no_budget() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 9, 5);
+    LocalDate endDate = LocalDate.of(2022, 9, 15);
+
+    // Act
+    int amount = budgetService.queryBudget(startDate, endDate);
+
+    // Assert
+    assertEquals(0, amount);
+  }
+
+  @Test
+  void test_no_cross_month() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 7, 5);
+    LocalDate endDate = LocalDate.of(2022, 7, 6);
+
+    givenBudgets(Arrays.asList(
+        new Budget("202206", 3000),
+        new Budget("202207", 3100),
+        new Budget("202208", 6200)
+    ));
+
+    // Act
+    int amount = budgetService.queryBudget(startDate, endDate);
+
+    // Assert
+    assertEquals(200, amount);
+  }
+
+  @Test
+  void test_no_cross_month_threshold() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 7, 1);
+    LocalDate endDate = LocalDate.of(2022, 7, 31);
+
+    givenBudgets(Arrays.asList(
+        new Budget("202206", 3000),
+        new Budget("202207", 3100),
+        new Budget("202208", 6200)
+    ));
+
+    // Act
+    int amount = budgetService.queryBudget(startDate, endDate);
+
+    // Assert
+    assertEquals(3100, amount);
+  }
+
+  @Test
+  void cross_month() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 6, 15);
+    LocalDate endDate = LocalDate.of(2022, 8, 10);
+
+    givenBudgets(Arrays.asList(
+        new Budget("202206", 3000),
+        new Budget("202207", 3100),
+        new Budget("202208", 6200)
+    ));
+
+    // Act
+    int amount = budgetService.queryBudget(startDate, endDate);
+
+    // Assert
+    assertEquals(6700, amount);
+  }
+
+  @Test
+  void cross_month_empty() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 6, 15);
+    LocalDate endDate = LocalDate.of(2022, 8, 10);
+
+    givenBudgets(Arrays.asList(
+        new Budget("202206", 3000),
+        new Budget("202208", 6200)
+    ));
+
+    // Act
+    int amount = budgetService.queryBudget(startDate, endDate);
+
+    // Assert
+    assertEquals(3600, amount);
+  }
+
+  @Test
+  void cross_month_threshold() {
+    // Arrange
+    LocalDate startDate = LocalDate.of(2022, 7, 31);
+    LocalDate endDate = LocalDate.of(2022, 8, 1);
+
+    givenBudgets(Arrays.asList(
+        new Budget("202206", 3000),
+        new Budget("202207", 3100),
+        new Budget("202208", 6200)
+    ));
+
+    // Act
+    int amount = budgetService.queryBudget(startDate, endDate);
+
+    // Assert
+    assertEquals(300, amount);
+  }
+
+  private void givenBudgets(List<Budget> budgets) {
+    Mockito.when(budgetRepo.findAll()).thenReturn(budgets);
+  }
 }
