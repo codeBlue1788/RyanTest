c17826d3dfa1c05d94e401bec906a29bf1a4172c
Ryan Lee <ryan.lee@hy-tech.com.tw>
2023-02-07 17:07:22 +0800

    tennis refactor

--- src/main/java/org/example/tdd/tennis/Tennis.java
+++ src/main/java/org/example/tdd/tennis/Tennis.java
@@ -1,5 +1,8 @@
 package org.example.tdd.tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
 
   private String firstPlayer;
@@ -15,15 +18,85 @@ public class Tennis {
     this.secondPlayer = secondPlayer;
   }
 
-  public void addFirstPlayerPoint(){
+  public void addFirstPlayerPoint() {
     this.firstPlayerPoint++;
   }
 
-  public void addSecondPlayerPoint(){
+  public void addSecondPlayerPoint() {
     this.secondPlayerPoint++;
   }
 
-  public String score(){
-    return null;
+  public String score() {
+    Map<Integer, String> lookUp = this.givenLookUp();
+
+    if (isEven()) {
+      if (isBothReachGamePoint()) {
+        return "deuce";
+      }
+      return getResult(lookUp, "all");
+    }
+
+    if (isBothReachGamePoint()) {
+      if (isDifferenceIs2()) {
+        return getResult("win");
+      }
+      return getResult("adv");
+    }
+
+    if (isAnyPlayerReach4()) {
+      return getResult("win");
+    }
+    return getResult(lookUp);
+  }
+
+  private boolean isBothReachGamePoint() {
+    return isFirstPlayerReachGamePoint() && isSecondPlayerReachGamePoint();
+  }
+
+  private boolean isAnyPlayerReach4() {
+    return this.firstPlayerPoint == 4 || this.secondPlayerPoint == 4;
+  }
+
+  private boolean isDifferenceIs2() {
+    return Math.abs(this.firstPlayerPoint - this.secondPlayerPoint) == 2;
+  }
+
+  private String getAdvPlayer() {
+    return this.firstPlayerPoint > this.secondPlayerPoint ? this.firstPlayer
+        : this.secondPlayer;
+  }
+
+  private String getResult(String phrase) {
+    return getAdvPlayer() + " " + phrase;
+  }
+
+  private String getResult(Map<Integer, String> lookUp, String phrase) {
+    return lookUp.get(this.firstPlayerPoint) + " " + phrase;
+  }
+
+  private String getResult(Map<Integer, String> lookUp) {
+    return lookUp.get(this.firstPlayerPoint) + " " + lookUp.get(this.secondPlayerPoint);
+  }
+
+  private boolean isFirstPlayerReachGamePoint() {
+    return this.firstPlayerPoint >= 3;
+  }
+
+  private boolean isSecondPlayerReachGamePoint() {
+    return this.secondPlayerPoint >= 3;
+  }
+
+
+  private Map<Integer, String> givenLookUp() {
+    Map<Integer, String> lookUp = new HashMap<>();
+    lookUp.put(0, "love");
+    lookUp.put(1, "fifteen");
+    lookUp.put(2, "thirty");
+    lookUp.put(3, "forty");
+    return lookUp;
+  }
+
+  private boolean isEven() {
+    return this.firstPlayerPoint == this.secondPlayerPoint;
   }
 }
--- src/test/java/test/tdd/tennis/TennisTest.java
+++ src/test/java/test/tdd/tennis/TennisTest.java
@@ -1,5 +1,116 @@
 package test.tdd.tennis;
 
-public class TennisTest {
+import org.example.tdd.tennis.Tennis;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TennisTest {
+
+  private Tennis tennis;
+
+  @BeforeEach
+  void setUp() {
+    tennis = new Tennis("Tom", "Jerry");
+  }
+
+  @Test
+  void love_all() {
+    // Arrange
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("love all", score);
+  }
+
+  @Test
+  void fifteen_all() {
+    // Arrange
+    addFirstPlayerPoint(1);
+    addSecondPlayerPoint(1);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("fifteen all", score);
+  }
+
+  @Test
+  void thirty_all() {
+    // Arrange
+    addFirstPlayerPoint(2);
+    addSecondPlayerPoint(2);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("thirty all", score);
+  }
+
+  @Test
+  void deuce() {
+    // Arrange
+    addFirstPlayerPoint(3);
+    addSecondPlayerPoint(3);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("deuce", score);
+  }
+  @Test
+  void fifteen_love() {
+    // Arrange
+    addFirstPlayerPoint(1);
+    addSecondPlayerPoint(0);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("fifteen love", score);
+  }
+
+  @Test
+  void Tom_adv() {
+    // Arrange
+    addFirstPlayerPoint(4);
+    addSecondPlayerPoint(3);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("Tom adv", score);
+  }
+
+
+  @Test
+  void Tom_win() {
+    // Arrange
+    addFirstPlayerPoint(4);
+    addSecondPlayerPoint(1);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("Tom win", score);
+  }
+
+  @Test
+  void Tom_win2() {
+    // Arrange
+    addFirstPlayerPoint(5);
+    addSecondPlayerPoint(3);
+    // Act
+    String score = tennis.score();
+    // Assert
+    Assertions.assertEquals("Tom win", score);
+  }
+
+
+  private void addSecondPlayerPoint(int point) {
+    for (int i = 0; i < point; i++) {
+    tennis.addSecondPlayerPoint();
+    }
+  }
+
+  private void addFirstPlayerPoint(int point) {
+    for (int i = 0; i < point; i++) {
+      tennis.addFirstPlayerPoint();
+    }
+  }
 
 }
